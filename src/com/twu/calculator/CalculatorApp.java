889d6e5adacfdd1c0d9e032c0e86322a8fc2db19
H4rryp0tt3r <nagesh.podilapu@gmail.com>
2015-06-11 11:36:43 +0530

    Jaswanth/Nagesh - Added specification for CalculatorApp

--- src/com/twu/calculator/CalculatorApp.java
+++ src/com/twu/calculator/CalculatorApp.java
@@ -3,14 +3,40 @@ package com.twu.calculator;
 import java.util.Scanner;
 
 public class CalculatorApp {
+
+    private Calculator calculator;
+
+    public CalculatorApp(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void start(boolean runForever) {
+        Scanner inputReader = new Scanner(System.in);
+        do {
+            System.out.print("> ");
+            String userInput = inputReader.nextLine();
+            if (isOperationNeedExternalOperator(userInput))
+                System.out.println(calculator.doOperation(getOperation(userInput), getOperand(userInput)));
+            else
+                System.out.println(calculator.doOperation(getOperation(userInput), 0));
+        } while (runForever);
+    }
+
+    private boolean isOperationNeedExternalOperator(String userInput) {
+        return (userInput.split(" ").length == 2) ? true : false;
+    }
+
+    private String getOperation(String userInput) {
+        return userInput.split(" ")[0];
+    }
+
+    private double getOperand(String userInput) {
+        return Double.parseDouble(userInput.split(" ")[1]);
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        Scanner input = new Scanner(System.in);
-        String command;
-        while (true) {
-            System.out.print("> ");
-            command = input.nextLine();
-            System.out.println(calculator.processCommand(command));
-        }
+        CalculatorApp calculatorApp = new CalculatorApp(calculator);
+        calculatorApp.start(true);
     }
 }
